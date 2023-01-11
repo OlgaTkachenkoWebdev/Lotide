6a2b5c865444df80fc86f4101463b4a3a7ac96a8
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-11 00:56:56 -0800

    function that removes elements from the array without changing the source

---
+++ without.js
@@ -0,0 +1,30 @@
+const assertArraysEqual = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    console.log(`🛑🛑🛑Assertion Failed:  ${actual} === ${expected}`);
+    process.exit();
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑Assertion Failed:  ${actual} === ${expected}`);
+      process.exit();
+    }
+  }
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+};
+
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (itemsToRemove.includes(source[i]) === false) {
+      newArray.push(source[i]);
+    }
+  }
+  return newArray;
+};
+
+// console.log(without([1, 2, 3], [1])) // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+// const words = ["hello", "world", "lighthouse"];
+// console.log(without(words, ["lighthouse"])) // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
