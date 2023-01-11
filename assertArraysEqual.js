4ceb54ad0c1fbdb6a10dc38b6b50822f8bfb5404
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-11 00:23:11 -0800

    Asserting Arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    console.log(`🛑🛑🛑Assertion Failed:  ${actual} === ${expected}`);
+    process.exit();
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑Assertion Failed:  ${actual} === ${expected}`);
+      process.exit();
+    }
+  }
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
