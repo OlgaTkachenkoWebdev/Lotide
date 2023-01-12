7adac3b943c1fc9f914b2931184a38c9a61df8a7
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-11 22:44:05 -0800

    positions of the letters

---
+++ letterPositions.js
@@ -0,0 +1,52 @@
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [ i ];
+      }
+    }
+  }
+  return results;
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed:  ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    console.log(`🛑🛑🛑Assertion Failed:  ${actual} === ${expected}`);
+    process.exit();
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑Assertion Failed:  ${actual} === ${expected}`);
+      process.exit();
+    }
+  }
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+};
+
+
+
+assertArraysEqual(letterPositions("hello").e, [1]);
