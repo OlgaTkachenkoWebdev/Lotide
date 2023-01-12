62fc0cf545593ad494e77303bb4d1db01a0cbc50
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-11 22:16:57 -0800

    counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+let countLetters = function(string) {
+  let countLetters = {};
+  for (let letter of string) {
+    if (letter !== " ") {
+      if (countLetters[letter]) {
+        countLetters[letter] += 1;
+      } else {
+        countLetters[letter] = 1;
+      }
+    }
+  }
+  return countLetters;
+};
+
+console.log(countLetters("lighthouse in the house"));
