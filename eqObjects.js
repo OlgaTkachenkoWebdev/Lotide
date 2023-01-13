2b508c125022bcd8e9ba90c654f6ffb35529d52d
woodenonesie <113571689+woodenonesie@users.noreply.github.com>
2023-01-12 23:28:11 -0800

    Update eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,5 @@
 // Returns true if both objects have identical keys with identical values(even when values are arrays).
+
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
