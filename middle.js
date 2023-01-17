6704fe97562ac20fcd23bb1b74fe3a878674acda
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-16 18:00:11 -0800

    middle refactored, added middle test

--- middle.js
+++ middle.js
@@ -1,22 +1,18 @@
-// returns array of numbers from the middle of a given array
-
 const middle = function(array) {
   let midArray = [];
   if (array.length <= 2) {
     return midArray;
   } else if (array.length % 2 === 0) {
     let midEven = array.slice((array.length / 2) - 1, (array.length / 2) + 1);
-    midArray.push(midEven);
+    midArray = midEven;
   } else {
     let midOdd = array[(array.length - 1) / 2];
     midArray.push(midOdd);
   }
+  console.log(midArray)
   return midArray;
 };
 
-// console.log(middle([1])) // => []
-// console.log(middle([1, 2])) // => []
-// console.log(middle([1, 2, 3])) // => [2]
-// console.log(middle([1, 2, 3, 4, 5])) // => [3]
-// console.log(middle([1, 2, 3, 4])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+middle([1, 2, 3, 4])
+
+module.exports = middle;
